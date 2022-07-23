1369e142f6335e12fba5e657ff9e3409bcddaf69
Tomás Pacheco <69006854+tomas-pacheco@users.noreply.github.com>
2022-07-23 17:03:06 -0300

    Comento los dos procesos de este modelo
    Este modelo solo tiene 2 procesos: el de extracción de la proyección y el de la reproyección. Comento estos dos.

--- model2.py
+++ model2.py
@@ -1,35 +1,49 @@
 """
-Model exported as python.
-Name : model2
-Group : 
-With QGIS : 32209
-"""
+Herramientas Computacionales para la Investigación - MAE UdeSA 2022
+Tomás Pacheco y Abigail Riquelme
+Comentarios sobre el modelo 2
 
-from qgis.core import QgsProcessing
-from qgis.core import QgsProcessingAlgorithm
-from qgis.core import QgsProcessingMultiStepFeedback
-from qgis.core import QgsProcessingParameterRasterDestination
-from qgis.core import QgsCoordinateReferenceSystem
+"""
+from qgis.core import (QgsProcessing, QgsProcessingAlgorithm, QgsProcessingMultiStepFeedback,
+                       QgsProcessingParameterRasterDestination,QgsCoordinateReferenceSystem)
 import processing
 
 
-class Model2(QgsProcessingAlgorithm):
+# Ahora vamos a definir el directorio de trabajo y las direcciones a las carpetas dentro
 
+main = "" # Definimos el directorio principal
+inputhdr = "{}/input/suit/hdr.adf".format(main) # Generamos un string con la direccion del archivo hdr.adf
+output = "{}/output".format(main) # Generamos un string con la dirección de la carpeta en donde guardaremos todo lo que exportemos
+
+
+# Definimos la clase para este modelo 2.
+class Model2(QgsProcessingAlgorithm):
+    
+    # Tal como hicimos antes, definimos los parámetros que vamos a usar. Este modelo solo va a tener
     def initAlgorithm(self, config=None):
         self.addParameter(QgsProcessingParameterRasterDestination('Suitout', 'suitout', createByDefault=True, defaultValue=None))
 
+    # Definimos el algoritmo para procesar las capas.
     def processAlgorithm(self, parameters, context, model_feedback):
         # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
         # overall progress through the model
         feedback = QgsProcessingMultiStepFeedback(2, model_feedback)
+        # Definimos dos diccionarios en los que se irá guardando el output
         results = {}
         outputs = {}
 
-        # Warp (reproject)
+        
+        #########################################################
+        # Vamos a usar la función para reproyectar la capa
+        #########################################################
+
+        # Lo que hacemos es definir los parámetros, como el input del archivo.
+        # Además, indicamos que queremos reproyectar a WGS84 con el sistema de coordenadas 'EPSG:4326'.
+        # Finalmente, indicamos el output.
         alg_params = {
             'DATA_TYPE': 0,  # Use Input Layer Data Type
             'EXTRA': '',
-            'INPUT': 'C:/Users/Abi/Desktop/herramientasC5/input/suit/hdr.adf',
+            'INPUT': inputhdr,
             'MULTITHREADING': False,
             'NODATA': None,
             'OPTIONS': '',
@@ -41,20 +55,33 @@ class Model2(QgsProcessingAlgorithm):
             'TARGET_RESOLUTION': None,
             'OUTPUT': parameters['Suitout']
         }
+        
+        # Aplicamos el proceso y guardamos en los diccionarios de output.
         outputs['WarpReproject'] = processing.run('gdal:warpreproject', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         results['Suitout'] = outputs['WarpReproject']['OUTPUT']
 
+        # A continuación indicamos que si ocurre algún tipo de error se suspensa el procesamiento del algoritmo y se devuelva un diccionario en blanco. 
         feedback.setCurrentStep(1)
         if feedback.isCanceled():
             return {}
 
-        # Extract projection
+        #########################################################
+        # Extraemos la proyección
+        #########################################################
+        
+        # Tenemos que extraer la proyección. Definimos los parámetros: el input es la capa exportada en el proceso anterior.
         alg_params = {
             'INPUT': outputs['WarpReproject']['OUTPUT'],
             'PRJ_FILE_CREATE': True
         }
+        
+        # Definimos que el output de la extracción de la proyección se guarde en el diccionario 'output'
         outputs['ExtractProjection'] = processing.run('gdal:extractprojection', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        return results
+       
+       # La función devuelve al diccionario llamado 'results' 
+       
+       return results
+
 
     def name(self):
         return 'model2'
